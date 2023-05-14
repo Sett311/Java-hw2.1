d078984040cb437f3345cf7deb70ca6c857727b0
Sett311 <2004sett@gmail.com>
2023-05-14 14:57:57 +0300

    Написана программа + убраны лишние папки и файлы

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    int cost = 250;
+    int mile = cost / 20;
+
+        System.out.println("У вас на счету " + mile + " миль(я)");
+
+
+    }
+}
